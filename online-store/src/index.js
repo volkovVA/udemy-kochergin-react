040403ce9c0934f362268795d1107379213fa0c6
volkovVA <volkov.slava@hotmail.com>
2022-03-20 19:30:57 +0300

    feat: add project setup

--- online-store/src/index.js
+++ online-store/src/index.js
@@ -1,3 +1,29 @@
 import './main.css'
 
-document.write('Hello React/Redux!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {applyMiddleware, createStore} from 'redux'
+import {createBrowserHistory} from 'history'
+import thunk from 'redux-thunk'
+import {ConnectedRouter, routerMiddleware} from 'connected-react-router'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {Provider} from 'react-redux'
+
+import createRootReducer from 'reducers'
+import Layout from 'containers/layout'
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
+const store = createStore(
+  createRootReducer(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Layout />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
---
+++ online-store/src/node_modules/containers/phones/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Phones = () => (
+  <div>Phones</div>
+)
+
+export default Phones
