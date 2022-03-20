63e46ce8e0359b167a83ae96cf6b45a4aa23016c
volkovVA <volkov.slava@hotmail.com>
2022-03-20 20:49:19 +0300

    feat: add rendering a page with products

---
+++ online-store/src/node_modules/selectors.js
@@ -0,0 +1,8 @@
+import * as R from 'ramda'
+
+export const getPhoneById = (state, id) => R.prop(id, state.phones)
+
+export const getPhones = state => {
+  const phones = R.map(id => getPhoneById(state, id), state.phonesPage.ids)
+  return phones
+}
